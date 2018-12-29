c03623166f63f2759fbdfc942f31186957628d4d
susmitarudrapal123 <46236097+susmitarudrapal123@users.noreply.github.com>
2018-12-29 14:30:16 +0530

    Add files via upload

---
+++ assi14.py
@@ -0,0 +1,8 @@
+#qus no-14 by susmita rudra pal
+from collections import Counter
+z=[]
+x=int(input("Enter the number of inputs u want to give"))
+for y in range(x):
+    a=int(input("Enter the number"))
+    z.append(a)
+Counter(z)
