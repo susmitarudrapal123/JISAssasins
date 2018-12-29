27f16f731c4869206f5debe92ebfbf73e05f7de0
susmitarudrapal123 <46236097+susmitarudrapal123@users.noreply.github.com>
2018-12-29 12:46:02 +0530

    Add files via upload

---
+++ assi11.py
@@ -0,0 +1,5 @@
+#qu no-11 by susmita rudra pal
+str=input("Enter the string")
+char=input("Enter the character whose number of occurences is to be found in the string")
+c=str.count(char)
+print("Number of occurences of {} is {}".format(char,c))
