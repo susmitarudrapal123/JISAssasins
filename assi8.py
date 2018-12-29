a1960266bc0315cf5d9fb64e1bb761ec33979fc9
susmitarudrapal123 <46236097+susmitarudrapal123@users.noreply.github.com>
2018-12-29 12:31:27 +0530

    Add files via upload

---
+++ assi8.py
@@ -0,0 +1,11 @@
+#qu no-8 by susmita rudra pal
+x = 5
+y = 10
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
