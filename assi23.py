013923ffd79489b566feccade7cae92d08f88deb
susmitarudrapal123 <46236097+susmitarudrapal123@users.noreply.github.com>
2018-12-29 15:37:58 +0530

    Add files via upload

---
+++ assi23.py
@@ -0,0 +1,15 @@
+#qus no-23 by susmita rudra pal
+X = input("Enter your string")
+l=len(X)
+print("The length is", l)
+Y = "Ankita"
+Z= X+Y
+print("After concatenation", Z)
+print(''.join(reversed(X)))
+old="I like Python"
+new=old.replace("like","love")
+print(new)
+if X == Y:
+    print("Equal")
+else:
+    print("Not Equal")
